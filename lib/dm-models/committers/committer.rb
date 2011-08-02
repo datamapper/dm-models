9674294d98a74362f0268be493f2bf91a25857d2
Ted Han <ted@knowtheory.net>
2011-08-02 01:09:19 -0400

    Renaming some methods, and de-dkubb-ifying the methods.

--- lib/dm-models/committers/committer.rb
+++ lib/dm-models/committers/committer.rb
@@ -12,22 +12,22 @@ module Committers
 
     # @return [Integer]
     #   degrees of separation from Paul Erdös
-    def dkubb_number(limit=99)
-      (0..limit).find { |depth| self.my_dkubb_number?(depth) }
+    def degrees_of_separation_from(person,limit=99)
+      (0..limit).find { |limit| self.separated_by?(person, limit) }
     end
 
     # @return [Boolean]
     #   is +depth+ the degrees of separation between receiver and Paul Erdös?
-    def my_dkubb_number?(depth)
+    def separated_by?(person, distance)
       # produces well defined query, with a repeating relation
       # where depth defines the number of repetitions
 
-      committers_at_depth = Committer.all(:name => "Dan Kubb")
-      return true if committers_at_depth.include?(self)
+      committers_at_distance = person
+      return true if committers_at_distance.include?(self)
 
-      depth.times { |i| committers_at_depth = committers_at_depth.coauthors }
+      distance.times { |i| committers_at_distance = committers_at_distance.cocontributors("id.not" => self.id) }
 
-      committers_at_depth.collaborators.include?(self) # return true or false
+      committers_at_distance.collaborators.include?(self) # return true or false
     end
   end
 end
